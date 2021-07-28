2f13f5165e36e654399c6df1524c96bafcc14c59
Mao <maokitty23k@gmail.com>
2021-07-28 14:49:44 -0400

    added icons

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import {useState, useEffect} from 'react';
 import firebase from './firebase';
 import './App.css';
 import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBirthdayCake, faUsers, faImage, faDoorClosed, faFlagCheckered } from '@fortawesome/free-solid-svg-icons';
 // import Login from './Login';
+
 
 function App() {
 
   const [textContent, setTextContent] = useState([]);
   const [userInput, setUserInput] = useState("");
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
-  setIsUserLoggedIn(false);
-
-  
 
   // Get info from Firebase
   useEffect(() => {
@@ -32,6 +31,7 @@ function App() {
     })
   }, []);
 
+  // Change, submit, delete
   const handleChange = (e) => {
     setUserInput(e.target.value)
   };
@@ -40,10 +40,7 @@ function App() {
     e.preventDefault();
     const dbRef = firebase.database().ref();
     dbRef.push(userInput);
-    console.log(userInput);
-
     setUserInput("");
-
   };
 
   return (
@@ -51,26 +48,23 @@ function App() {
 
       <div className="container">
       <nav>
-        <p>MENU</p>
-        <p><a href="#usertText">Profile</a></p>
-        <p><a href="#usertText">Aniversary</a></p>
-        <p><a href="#usertText">Album</a></p>
-        <p><a href="#usertText">Room</a></p>
-        <p><a href="#usertText">Notification</a></p>
+        <p className="menu">MENU</p>
+        <p><a href="#usertText"><FontAwesomeIcon icon={faUsers} /> Profile</a></p>
+        <p><a href="#usertText"><FontAwesomeIcon icon={faBirthdayCake} /> Aniversary</a></p>
+        <p><a href="#usertText"><FontAwesomeIcon icon={faImage} /> Album</a></p>
+        <p><a href="#usertText"><FontAwesomeIcon icon={faDoorClosed} /> Room</a></p>
+        <p><a href="#usertText"><FontAwesomeIcon icon={faFlagCheckered} /> Notification</a></p>
       </nav>
 
       <div className="container2">
         <header className="App-header">
           <div className="wrapper">
-            {
-              isUserLoggedIn ? <p>Welcome to the App!</p> : null
-            }
             <h1>LINK'd</h1>
             <p>Share your feeling with someone   important</p>
-            <form action="submit" onSubmit=  {handleSubmit}>
+            <form action="submit" onSubmit={handleSubmit}>
               <label htmlFor="userText"></label>
-              <textarea id="userText"  onChange=  {handleChange} value={userInput} cols="70" rows="10" placeholder="What's happening" maxlength="180"></textarea>
-              <button type="submit">Tweet!</button>
+              <textarea id="userText" onChange={handleChange} value={userInput} cols="70" rows="10" placeholder="What's happening" maxlength="180"></textarea>
+              <button type="submit">Update!</button>
             </form>
           </div>
         </header>
@@ -96,7 +90,9 @@ function App() {
 
 
       <footer className="App-footer">
-        <p>Created at<a href="https://junocollege.com/">Juno College of Techonology</a> by Mao</p>
+        <div className="wrapper">
+          <p>Created at<a href="https://junocollege.com/">Juno College of Techonology</a> by Mao</p>
+        </div>
       </footer>
     </div>
   );
